a8a8aedaa93517841bb21da76af8af1b1d28f9b7
Karishma2422 <karishmapatil74@gmail.com>
2024-10-01 12:59:04 +0530

    day 1

---
+++ day 1.js/index.js
@@ -0,0 +1,57 @@
+// 1. Datatypes
+// String
+// Number
+// Boolean
+// undefined
+// null
+// 2. Varible
+// var
+// let
+// const
+
+
+
+
+// 1.1 String
+// "" '' ``
+
+// "awdiz"
+// 'vashi'
+// `students`
+// '1'
+// "234"
+// `3245#@$%^&#@#$`
+
+// function -> multiple lines of code -> feature return
+
+console.log("KARISHMA")
+console.log(typeof('PANVEL'))
+console.log(typeof(`I AM DEVLOPER`))
+console.log(typeof("12345"))
+console.log(typeof('56789'))
+console.log(typeof(`!@#$%^&*`))
+
+// Number
+
+// 12
+// 2121
+// 456789
+// console.log(typeof(a))
+// console.log(typeof(2.23))
+// console.log(typeof("@@#$$"))
+
+// Boolean true false
+
+// console.log(typeof(true))
+// console.log(typeof('false'))
+
+// Assign =
+// re assign  =
+// declare =
+
+// var - store data
+
+// difference between var let and const
+//                 var
+// reassign        true
+// redeclare       true
