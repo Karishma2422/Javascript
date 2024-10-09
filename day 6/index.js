2a4032330d5116e0480b70f39128a695312176b1
Karishma2422 <karishmapatil74@gmail.com>
2024-10-09 15:23:04 +0530

    day 6

---
+++ day 6/index.js
@@ -0,0 +1,131 @@
+
+
+for (var myNumber = 1; myNumber < 11; myNumber++) {
+    console.log(myNumber); // 1
+  }
+  
+// 1 Iteration : 
+// var myNumber = 1;
+// myNumber < 11
+// 1 < 11 => true 
+// log(myNumber) => 1
+// myNumber++
+// myNumber = 2
+
+// 2nd Iteration :
+// myNumber = 2
+// myNumber < 11
+// 2 < 11 => true
+// log(myNumber) => 2
+// myNumber++
+// myNumber = 3
+
+// 3rd iteration 
+// myNumber = 3
+// myNumber < 11
+// 3 < 11 => true 
+// log(myNumber);  3
+// myNumber++
+// myNumber = 4
+
+// 4 th iteration 
+// myNumber = 4;
+// myNumber < 11
+// 4 < 11 => true 
+// log(myNumber) // 4
+// myNumber++; 
+// myNumber = 5
+
+   // 5 th Iteration : 
+// mynumber =5;
+// myNumber < 11
+// 5 < 11 => true 
+// log(myNumber) //5
+// myNumber++;
+// myNumber = 6
+
+// 6 th Iteration :
+// myNumber = 6
+// myNumber < 11
+// 6 < 11 => true
+// log(myNumber) //6
+// myNumber++
+// myNumber = 7
+
+// 7th iteration 
+// myNumber = 7
+// myNumber < 11
+// 7 < 11 => true 
+// log(myNumber) // 7
+// myNumber++
+// myNumber = 8
+
+// 8 th iteration 
+// myNumber = 8;
+// myNumber < 11
+// 4 < 11 => true 
+// log(myNumber) // 8
+// myNumber++; 
+// myNumber = 9
+
+   // 9 th iteration 
+// myNumber = 9;
+// myNumber < 11
+// 4 < 11 => true 
+// log(myNumber) // 9
+// myNumber++; 
+// myNumber = 10
+
+  // 10 th iteration 
+  // myNumber = 9;
+  // myNumber < 11;
+  // 9 < 11 true 
+  // log(myNumber) 9
+  // myNumber++
+  // myNumber = 10;
+  
+  // 11 th iteration 
+  // myNumber = 10;
+  // myNumber < 11
+  // 10 < 11 true 
+  // log(myNumber) 10 
+  // myNumber++
+  // myNumber = 11;
+  
+  // 12 iteration 
+  
+  // myNumber = 11;
+  // myNumber < 11;
+  // 11 < 11 => false 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // var num = 17;
+  // num--; // 16
+  // num += 2; // 18
+  // num++; // 19
+  // num = num - 1; // 18
+  // num -= 1; // 17
+  // num = num + 4;
+  // num = num - 2;
+  // num++; // increment operator -  it'll increment value by 1
+  // num++; // it'll increment value by 1
+  // num--;
+  // num--;
+  // num += 3;
+  // num -= 3;
+  
+  // console.log(num);
