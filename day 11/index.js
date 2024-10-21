d75b1f8b99365bbd54d598cb74799f9d2618d8f5
Karishma2422 <karishmapatil74@gmail.com>
2024-10-21 11:46:30 +0530

    day 11

---
+++ day 11/index.js
@@ -0,0 +1,11 @@
+var array =[5,4,8,9,2,3,1,45,79,23,89];
+var target = 5;
+for (var j = 0; j < array.length; j++){
+    //console.log(j);
+    //console.log(array[j]);
+    if (array[j]---target){
+        console.log(j)
+    }
+}
+
+//binary asending
