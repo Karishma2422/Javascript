b1799d849fbd24b46c892ed00bbd19e7db25f47f
Karishma2422 <karishmapatil74@gmail.com>
2024-10-26 16:47:24 +0530

    day 12

--- day 12/index.js
+++ day 12/index.js
@@ -1,28 +1,28 @@
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var target = 5;
+var target = 75;
 
 
 
-function binarysearch(array,target){
+function binarysearch() {
 var left =0;
 var right = array.length - 1;
 
-while(left <= right){
+while (left <= right) {
     console.log(left, "left", right, "right" )
-    const midIndex = Math.floor((left + right /2))
+    const midIndex = Math.floor((left + right ) / 2);
        console.log(midIndex,"midIndex",array[midIndex], "element");
      if (array[midIndex] == target) {
         console.log(array[midIndex], target, "array[midIndex] == target");
         return midIndex;
      }else if (array[midIndex] > target){
+        right = midIndex - 1;
         console.log(array[midIndex], target, "array[midIndex] > target");
-        right = mid - 1;
      } else if (array[midIndex] < target){
+        left = midIndex + 1;
         console.log(array[midIndex], target, "array[midIndex] > target");
-        left = mid + 1;
      }
 
     }
-
+return "not found";
 }
 console.log(binarysearch(array,target));
